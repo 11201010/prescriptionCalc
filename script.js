8242b217ec4d21e28a83bf0eb5f6d103cb192809
11201010 <3a6l8j6l@gmail.com>
2022-11-05 22:01:29 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,88 @@
+let startDate = document.getElementById("startDate");
+let interval = document.getElementById("interval");
+let dispenseNum = document.getElementById("dispenseNum");
+let currentDate = document.getElementById("currentDate");
+let revisitDate = document.getElementById("revisitDate");
+let firstRange = document.getElementById("firstRange");
+let secondRange = document.getElementById("secondRange");
+let third = document.getElementById("third");
+let thirdRange = document.getElementById("thirdRange");
+let expireDate = document.getElementById("expireDate");
+let modifiedRange = document.getElementById("modifiedRange");
+let modifiedRevisit = document.getElementById("modifiedRevisit");
+let modifiedRevisitContainer = document.getElementById(
+	"modifiedRevisitContainer"
+);
+
+currentDate.valueAsDate = new Date();
+
+function prescription() {
+	firstRange.innerHTML =
+		moment(startDate.value).format("YYYY-MM-DD") +
+		" ~ " +
+		moment(startDate.value)
+			.add(10 - 1, "d")
+			.format("YYYY-MM-DD");
+	secondRange.innerHTML =
+		moment(startDate.value)
+			.add(interval.value - 10, "d")
+			.format("YYYY-MM-DD") +
+		" ~ " +
+		moment(startDate.value)
+			.add(interval.value - 1, "d")
+			.format("YYYY-MM-DD");
+	third.style.display = dispenseNum.value == 2 ? "none" : "block";
+	thirdRange.innerHTML =
+		moment(startDate.value)
+			.add(interval.value * 2 - 10, "d")
+			.format("YYYY-MM-DD") +
+		" ~ " +
+		moment(startDate.value)
+			.add(interval.value * 2 - 1, "d")
+			.format("YYYY-MM-DD");
+	expireDate.innerHTML = moment(startDate.value)
+		.add(interval.value * dispenseNum.value, "d")
+		.format("YYYY-MM-DD");
+	if (
+		currentDate.valueAsDate >
+		moment(startDate.value).add(interval.value * dispenseNum.value, "d")
+	) {
+		modifiedRange.innerHTML = "不可領藥";
+	} else {
+		let modifiedEnd = moment.min(
+			// expireDate
+			moment(startDate.value).add(interval.value * dispenseNum.value, "d"),
+			// modifiedDate + interval
+			moment(currentDate.value).add(interval.value, "d")
+		);
+		let suggestStart = moment.max(
+			// thirdStart
+			moment(startDate.value).add(interval.value * 2 - 10, "d"),
+			// modifiedDate + interval - 10
+			moment(currentDate.value).add(interval.value - 10, "d")
+		);
+		let suggestEnd;
+		// if currentDate > thirdEnd
+		if (
+			currentDate.valueAsDate >
+			moment(startDate.value).add(interval.value * 2 - 1, "d")
+		) {
+			suggestEnd = modifiedEnd;
+		} else {
+			suggestEnd = moment.min(
+				modifiedEnd,
+				moment(startDate.value).add(interval.value * 2 - 1, "d")
+			);
+		}
+		console.log(revisitDate.value == "");
+		modifiedRange.innerHTML =
+			suggestStart.format("YYYY-MM-DD") +
+			" ~ " +
+			suggestEnd.format("YYYY-MM-DD");
+		modifiedRevisit.innerHTML =
+			revisitDate.value < suggestStart.format("YYYY-MM-DD")
+				? "約診日期至少須為" + suggestStart.format("YYYY-MM-DD") + "以後"
+				: "否";
+	}
+}
+prescription();
